c8a88f0fd48cca179f13c5e6d5bd871ea427b2c9
Jaideep Singh <singhjaideep@users.noreply.github.com>
2015-08-06 15:24:55 -0500

    Multiline + added logic for shareadealtracker

--- removeTracker.js
+++ removeTracker.js
@@ -1,1 +1,14 @@
-javascript:(function(){thisurl=document.getElementsByClassName("what code")[0].innerHTML;substring=thisurl.search("url=");sliced=thisurl.slice([substring+4],thisurl.length);decoded=decodeURIComponent(sliced);alert(decoded);window.location.href=decoded})();
+javascript: (function() {
+  thisurl = document.getElementsByClassName("what code")[0].innerHTML;
+  alert(thisurl);
+  if (thisurl.search("http://www.shareasale.com/r.cfm?") != -1) {
+    substring = thisurl.search("ink=");
+  }
+  else {
+    substring = thisurl.search("url=");
+  }
+  sliced = thisurl.slice([substring + 4], thisurl.length);
+  decoded = decodeURIComponent(sliced);
+  alert(decoded);
+  window.location.href = decoded
+})();
